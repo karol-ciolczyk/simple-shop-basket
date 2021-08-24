a4bf6c31f3e86c648eea6209dcc577df7e1b2ce5
Karol Ciołczyk <dyweko@gmail.com>
2021-08-24 17:54:49 +0200

    styles added to ProductCard.js

--- shop-basket/src/components/ProductCard.js
+++ shop-basket/src/components/ProductCard.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import { CardActionArea, Grid } from "@material-ui/core";
 
 import { gql, useQuery } from "@apollo/client";
 
@@ -22,8 +22,17 @@ const GET_PRODUCTS = gql`
 `;
 
 const useStyles = makeStyles({
+  container: {
+    margin: "auto",
+    marginTop: "50px",
+    width: "80%",
+  },
   root: {
-    maxWidth: 345,
+    width: 250,
+    height: 280,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
   },
   media: {
     height: 140,
@@ -31,7 +40,8 @@ const useStyles = makeStyles({
   actionCard: {
     display: "flex",
     justifyContent: "space-around",
-    margin: "3px 0px",
+    margin: "10px 0px",
+    alignItems: "center",
   },
 });
 
@@ -45,27 +55,91 @@ export function ProductCard() {
   if (error) return <p>Error :(</p>;
 
   return (
-    <>
+    <Grid
+      container
+      justifyContent="center"
+      spacing={2}
+      className={classes.container}
+    >
       {data.products.map((product) => (
-        <Card className={classes.root}>
-          <CardMedia
-            className={classes.media}
-            image={product.image}
-            title="Contemplative Reptile"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              {product.name}
-            </Typography>
-          </CardContent>
-          <div className={classes.actionCard}>
-            <Typography> Price: {product.price}</Typography>
-            <Button size="small" variant="contained" color="primary">
-              add to basket
-            </Button>
-          </div>
-        </Card>
+        <Grid key={product.id} item>
+          <Card className={classes.root}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image={product.image}
+                title="Contemplative Reptile"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h7" component="h2">
+                  {product.name}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <div className={classes.actionCard}>
+              <Typography variant="subtitle2">
+                Price: {product.price}
+              </Typography>
+              <Button size="small" variant="contained" color="primary">
+                add to basket
+              </Button>
+            </div>
+          </Card>
+        </Grid>
       ))}
-    </>
+    </Grid>
   );
 }
+
+{
+  /* <Grid container justifyContent="center" spacing={spacing}>
+  {data.products.map((product) => (
+    <Grid key={product.id} item>
+      <Card className={classes.root}>
+        <CardMedia
+          className={classes.media}
+          image={product.image}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {product.name}
+          </Typography>
+        </CardContent>
+        <div className={classes.actionCard}>
+          <Typography> Price: {product.price}</Typography>
+          <Button size="small" variant="contained" color="primary">
+            add to basket
+          </Button>
+        </div>
+      </Card>
+    </Grid>
+  ))}
+</Grid>; */
+}
+
+// return (
+//     <>
+//       {data.products.map((product) => (
+//         <Card className={classes.root} key={product.id}>
+//           <CardMedia
+//             className={classes.media}
+//             image={product.image}
+//             title="Contemplative Reptile"
+//           />
+//           <CardContent>
+//             <Typography gutterBottom variant="h5" component="h2">
+//               {product.name}
+//             </Typography>
+//           </CardContent>
+//           <div className={classes.actionCard}>
+//             <Typography> Price: {product.price}</Typography>
+//             <Button size="small" variant="contained" color="primary">
+//               add to basket
+//             </Button>
+//           </div>
+//         </Card>
+//       ))}
+//     </>
+//   );
+// }
